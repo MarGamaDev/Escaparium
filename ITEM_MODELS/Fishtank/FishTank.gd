e530d100cea5f9d05708aec54a326cedf903241c
MarGama <94371820+MarGamaDev@users.noreply.github.com>
2025-12-21 18:54:31 +0100

    turns off fish jump test function

--- ITEM_MODELS/Fishtank/FishTank.gd
+++ ITEM_MODELS/Fishtank/FishTank.gd
@@ -3,6 +3,5 @@ extends AnimatableBody3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer;
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("Jump"):
-		animation_player.play("send_off_to_war");
+func play_jump_animation() -> void:
+	animation_player.play("send_off_to_war");

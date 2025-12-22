d85c40d2ecc91303afc18429372402ac1f46b528
MarGama <94371820+MarGamaDev@users.noreply.github.com>
2025-12-22 09:03:52 +0100

    sets up fishtank and cabinet

---
+++ ITEM_MODELS/Tank Cabinet/Cabinet.gd
@@ -0,0 +1,10 @@
+class_name Cabinet
+extends Node3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer;
+@onready var interactable: Interactable = $"CABINET DOOR/AnimatableBody3D";
+
+func on_interact() -> void:
+	print("opening")
+	animation_player.play("open");
+	interactable.prerequisite_strings.append("already_triggered");
